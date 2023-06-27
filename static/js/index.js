c10a752c939bc4e6b1c4249be2e404d1ea38404a
Khanh Phan Trần <ptrankhanh02@gmail.com>
2023-06-27 16:32:34 +0700

    deny non url link

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 let timeoutId;
 
-function keyUpHandler() {
+function summarizeTextHandler() {
   clearTimeout(timeoutId);
   if (this.value !== '') {
     timeoutId = setTimeout(summarizeNews('text'), 3000);
@@ -11,8 +11,19 @@ function keyPressHandler() {
   clearTimeout(timeoutId);
 }
 
-function summarizeNews(opt) {
+function summarizeURLHandler() {
+  var input = document.getElementById('url-input');
+  var url = input.value;
 
+  if (url.startsWith('https://e.vnexpress.net/news/')) {
+    summarizeNews('url');
+  } else {
+    var alertMessage = document.getElementById('url-failed-alert');
+    alertMessage.classList.remove('d-none');
+  }
+};
+
+function summarizeNews(opt) {
   /* BEGIN-Show Loading Progress */
   var outputElement = document.getElementById('text-output');
   var dots = '';
@@ -43,27 +54,6 @@ function summarizeNews(opt) {
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.send(JSON.stringify({ [opt == 'text' ? 'text' : 'url']: val }));
 }
-
-/* function summarizeURL() {
-  interval = showLoadingProgress();
-
-  var url = document.getElementById("url-input").value;
-  var xhr = new XMLHttpRequest();
-
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      clearInterval(interval);
-      var response = JSON.parse(xhr.responseText);
-      document.getElementById("text-output").textContent = response.summary;
-    } else if (xhr.readyState === 4) {
-      console.error(xhr.responseText);
-    }
-  };
-
-  xhr.open("POST", "/op=url", true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.send(JSON.stringify({ "url": url }));
-} */
 
 function autoExpand(element) {
   element.style.height = 'auto';
